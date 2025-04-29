b041fa05a40437bf43ee44242fc1e313e486b4ed
MoisesCorcho <mcorchoperez@gmail.com>
2025-04-28 21:34:12 -0500

    feat: social-icons updated, changes in home and hero sections and some cleanup.

--- resources/views/components/home/about.blade.php
+++ resources/views/components/home/about.blade.php
@@ -1,57 +1,56 @@
 <!-- ====== About Section Start -->
 <section id="about" class="dark:bg-slate-800 pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] overflow-hidden">
-  <div class="container">
-    <div class="flex flex-wrap justify-between items-center -mx-4">
-      <div class="w-full lg:w-6/12 px-4">
-        <div class="flex items-center -mx-3 sm:-mx-4">
-          <div class="w-full xl:w-1/2 px-3 sm:px-4">
-            <div class="py-3 sm:py-4">
-              <img
-                src="{{ url('/img/gohanTorneo.jpeg') }}"
-                alt=""
-                class="rounded-2xl w-full"
-              />
+    <div class="container">
+        <div class="flex flex-wrap justify-between items-center -mx-4">
+            <div class="w-full lg:w-6/12 px-4">
+                <div class="flex items-center -mx-3 sm:-mx-4">
+                    <div class="w-full xl:w-1/2 px-3 sm:px-4">
+                        <div class="py-3 sm:py-4">
+                            <img src="{{ url('/img/MoisesPiano.jpeg') }}" alt="" class="rounded-2xl w-full" />
+                        </div>
+                        <div class="py-3 sm:py-4">
+                            <img src="{{ url('/img/GataMascotaPc.jpeg') }}" alt="" class="rounded-2xl w-full" />
+                        </div>
+                    </div>
+                    <div class="w-full xl:w-1/2 px-3 sm:px-4">
+                        <div class="my-4 relative z-10">
+                            <img src="{{ url('/img/GataMascota.jpeg') }}" alt="" class="rounded-2xl w-full" />
+                            <x-about-dots></x-about-dots>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="py-3 sm:py-4">
-              <img
-                src="{{ url('/img/gohan2.JPG') }}"
-                alt=""
-                class="rounded-2xl w-full"
-              />
-            </div>
-          </div>
-          <div class="w-full xl:w-1/2 px-3 sm:px-4">
-            <div class="my-4 relative z-10">
-              <img
-                src="{{ url('/img/AdultSonGohan.jpg') }}"
-                alt=""
-                class="rounded-2xl w-full"
-              />
-              <x-about-dots></x-about-dots>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
-        <div class="mt-10 lg:mt-0">
-          <span class="font-semibold text-lg text-primary mb-2 block">
-             <blockquote class="text-sm text-gray-500 italic py-2 px-3 border-l-4 border-amber-500">
-                 "Not everything is achievable with hard work, but... It helps a lot"
-             </blockquote>
-          </span>
-          <h2 class="font-bold text-3xl sm:text-4xl dark:text-slate-200 mb-8">
-            {{__('Acerca de mi')}}
-          </h2>
-          <p class="text-base dark:text-slate-400 mb-8">
-            Soy un desarrollador web enfocado en backend con 4 años de experiencia formado tanto en entornos académicos como empresariales. Mi enfoque en <span class="text-amber-500 font-bold">PHP</span> y <span class="text-amber-500 font-bold">Laravel</span> se ha fortalecido a través de proyectos reales y personales. Mi pasión por el aprendizaje y la resolución lógica de problemas se combina con habilidades de empatía y escucha activa, fortaleciendo la colaboración en situaciones laborales desaﬁantes.
-          </p>
+            <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
+                <div class="mt-10 lg:mt-0">
+                    <span class="font-semibold text-lg text-primary mb-2 block">
+                        <blockquote class="text-sm text-gray-500 italic py-2 px-3 border-l-4 border-amber-500">
+                            "Not everything is achievable with hard work, but... It helps a lot"
+                        </blockquote>
+                    </span>
+                    <h2 class="font-bold text-3xl sm:text-4xl dark:text-slate-200 mb-8">
+                        {{ __('Acerca de mi') }}
+                    </h2>
+                    <p class="text-base dark:text-slate-400 mb-6">
+                        Mi formación como Ingeniero de Sistemas, culminada con honores, me ha dotado de una aguda
+                        capacidad para el análisis y la resolución lógica de problemas. El conocimiento que he adquirido
+                        en todo estos años permite construir soluciones robustas y eficientes. Consciente del valor del
+                        trabajo en equipo, mi naturaleza comprensiva y mi habilidad para escuchar activamente
+                        contribuyen a una dinámica colaborativa positiva y productiva.
+                    </p>
 
-          <x-button-link href="" variant="black" target="_blank">
-            Mira mi Github
-          </x-button-link>
+                    <p class="text-base dark:text-slate-400 mb-6">
+                        Cuando no estoy inmerso en el mundo de la programación, probablemente esté
+                        compartiendo mimos con mis gatos o dejándome llevar por la música escuchándola o tocándola.
+                        Estas pasiones, aunque personales, son parte de lo que me motiva y me aporta una perspectiva
+                        única a la hora de colaborar y resolver problemas.
+                    </p>
+
+                    <x-button-link href="" variant="black" target="_blank">
+                        Mira mi Github
+                    </x-button-link>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
 <!-- ====== About Section End -->
--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,85 +1,76 @@
 <!-- ====== Hero Section Start -->
 <div class="relative pt-[60px] lg:pt-[80px] pb-[110px] bg-white dark:bg-slate-900">
-  <div class="container">
-    <div class="flex flex-wrap -mx-4">
-      <div class="w-full lg:w-5/12 px-4">
-        <div class="hero-content">
-          <h1
-            class="text-dark dark:text-slate-200 font-bold text-4xl sm:text-[42px] lg:text-[40px] xl:text-[42px] leading-snug mb-6"
-          >
-            {{__('Mi nombre es')}} <br/>
-            <span class="rounded-md text-amber-600">Moisés Corcho Pérez</span>.
-          </h1>
-          <p class="text-base mb-8 max-w-[520px] dark:text-slate-200">
-            Soy un desarrollador web enfocado en backend con 4 años de experiencia con experticia en PHP, Laravel y bases de datos, especializado en resolver problemas lógicamente y colaborar eficazmente en entornos desafiantes.
-            {{-- <a
-              class="text-primary font-bold no-underline hover:underline underline-offset-2 decoration-2 decoration-primary"
-              href="" target="_blank">Gitbub</a>. <br>
-          </p> --}}
-          <ul class="flex flex-wrap items-center">
-            <li>
-              <x-button-link href="#portfolio" variant="primary" class="rounded-lg">
-                Mis proyectos
-              </x-button-link>
-            </li>
-          </ul>
-          <div class="clients pt-16">
-            <h6 class="font-normal text-xs flex items-center text-body-color dark:text-slate-300 mb-4">
-              Mis redes sociales
-              <span class="w-8 h-[1px] bg-body-color inline-block ml-2"></span>
-            </h6>
-            <x-social-icons></x-social-icons>
-          </div>
+    <div class="container">
+        <div class="flex flex-wrap -mx-4">
+            <div class="w-full lg:w-5/12 px-4">
+                <div class="hero-content">
+                    <h1
+                        class="text-dark dark:text-slate-200 font-bold text-4xl sm:text-[42px] lg:text-[40px] xl:text-[42px] leading-snug mb-6">
+                        {{ __('Mi nombre es') }} <br />
+                        <span class="rounded-md text-amber-600">Moisés Corcho Pérez</span>.
+                    </h1>
+                    <p class="text-base mb-8 max-w-[520px] dark:text-slate-200">
+                        Soy un desarrollador web enfocado en backend con 4 años de experiencia formado tanto en entornos
+                        académicos como empresariales. Mi enfoque en <span class="text-amber-500 font-bold">PHP</span> y
+                        <span class="text-amber-500 font-bold">Laravel</span> se ha fortalecido a través de proyectos
+                        reales y personales. Mi pasión por el aprendizaje y la resolución lógica de problemas se combina
+                        con habilidades de empatía y escucha activa, fortaleciendo la colaboración en situaciones
+                        laborales desaﬁantes.
+                    <ul class="flex flex-wrap items-center">
+                        <li>
+                            <x-button-link href="#portfolio" variant="primary" class="rounded-lg">
+                                Mis proyectos
+                            </x-button-link>
+                        </li>
+                    </ul>
+                    <div class="clients pt-16">
+                        <h6 class="font-normal text-xs flex items-center text-body-color dark:text-slate-300 mb-4">
+                            Mis redes sociales
+                            <span class="w-8 h-[1px] bg-body-color inline-block ml-2"></span>
+                        </h6>
+                        <x-social-icons></x-social-icons>
+                    </div>
+                </div>
+            </div>
+            <div class="hidden lg:block lg:w-1/12 px-4"></div>
+            <div class="w-full lg:w-6/12 px-4">
+                <div class="lg:text-right lg:ml-auto">
+                    <div class="relative inline-block z-10 pt-11 lg:pt-0">
+                        <img src="{{ url('/img/moises-1.jpg') }}" alt="hero" class="max-w-full lg:ml-auto" />
+                        <span class="absolute -left-8 -bottom-8 z-[-1]">
+                            <svg width="93" height="93" viewBox="0 0 93 93" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
+                                <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
+                                <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
+                                <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
+                                <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
+                                <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
+                                <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
+                                <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
+                                <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
+                                <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
+                                <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
+                                <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
+                                <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
+                                <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
+                                <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
+                                <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
+                                <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
+                                <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
+                                <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
+                                <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
+                                <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
+                                <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
+                                <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
+                                <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
+                                <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="hidden lg:block lg:w-1/12 px-4"></div>
-      <div class="w-full lg:w-6/12 px-4">
-        <div class="lg:text-right lg:ml-auto">
-          <div class="relative inline-block z-10 pt-11 lg:pt-0">
-            <img
-              src="{{ url('/img/moises-1.jpg') }}"
-              alt="hero"
-              class="max-w-full lg:ml-auto"
-            />
-            <span class="absolute -left-8 -bottom-8 z-[-1]">
-              <svg
-                width="93"
-                height="93"
-                viewBox="0 0 93 93"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3"/>
-              </svg>
-            </span>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </div>
 <!-- ====== Hero Section End -->
