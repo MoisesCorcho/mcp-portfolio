318e8bad8a91a0c43163446e630357423cec1a6c
MoisesCorcho <mcorchoperez@gmail.com>
2025-05-02 17:19:01 -0500

    feat: ProjectSeeder has been added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(ProjectSeeder::class);
     }
 }
